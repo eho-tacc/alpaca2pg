6711848dfa53f5a3b2f1308ffc72833fccaa544d
Ethan Ho <eho@tacc.utexas.edu>
2021-04-06 15:37:05 -0600

    Basic command line arguments

--- alpaca2pg/__main__.py
+++ alpaca2pg/__main__.py
@@ -1,10 +1,38 @@
+import argparse
 import petl
+import psycopg2
 
 
-def main():
+def get_pgconn(opts):
+    """Connect to remote DB using credentials passed in command line"""
+    kw = {k: getattr(opts, k) for k in 
+          ('dbname', 'user', 'password', 'host', 'port')}
+    return psycopg2.connect(**kw)
+
+
+def main(opts):
     """Main entrypoint function"""
-    pass
+    pgconn = get_pgconn(opts)
+    table = (
+        petl
+        # table = etl.fromdb(pgconn, 'SELECT * FROM example')
+    )
+
+
+def get_opts():
+    """Get command line options from argparse"""
+    p = argparse.ArgumentParser(
+        description=("Pull historical quotes from Alpaca REST API to "
+                     "PostgreSQL DB. Please see `pysopg2.connect` docs "
+                     "for details."))
+    p.add_argument('--dbname', type=str, required=True)
+    p.add_argument('--user', type=str, required=True)
+    p.add_argument('--password', type=str, required=True)
+    p.add_argument('--host', type=str, required=True)
+    p.add_argument('--port', type=int, required=True)
+    return p.parse_args()
 
 
 if __name__ == '__main__':
-    main()
+    opts = get_opts()
+    main(opts)
